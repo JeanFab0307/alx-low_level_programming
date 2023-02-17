c15331dbec86f51ed1ca2670e1860fe9775788ff
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-02-17 12:35:51 -0800

    A BIG challenge

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ *main - Emtry Point
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int num0;
+	int num1;
+	int num2;
+
+	num1 = '0';
+	while (num0 != '9' + 1)
+	{
+		while (num1 != '9' + 1)
+		{
+			if (num1 > num0)
+			{
+				while (num2 != '9' + 1)
+				{
+					if (num2 > num1)
+					{
+						putchar(num0);
+						putchar(num1);
+						putchar(num2);
+						if (num1 == '7')
+							break;
+						putchar(',');
+						putchar(' ');
+						num2++;
+					}
+					else
+					{
+						num2++;
+					}
+				}
+			}
+			else
+			{
+				num1++;
+			}
+			num2 = '0';
+			num1++;
+		}
+		num1 = '0';
+		num2++;
+	}
+	putchar('\n');
+	return (0);
+}
