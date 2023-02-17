203a816707923e9e6db7940a4303c06b5a79235e
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-02-17 10:34:06 -0800

    Another one

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: always 0
+*/
+
+int main(void)
+{
+	char letter;
+
+	while (letter != z)
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+	return (0);
+}
