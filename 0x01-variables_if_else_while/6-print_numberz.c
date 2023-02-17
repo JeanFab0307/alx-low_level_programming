4ada01a8f0db0c019ba283d501b35b10176a9e3f
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-02-17 11:22:06 -0800

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	num = '0';
+	while (num != '9' + 1 )
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
