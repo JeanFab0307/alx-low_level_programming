6af9cc8bd0101889f5e89e56ae39b762110e215f
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-02-20 09:04:21 -0800

    WHy Betty

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,38 +17,38 @@ int main(void)
 	num2 = '0';
 	while (num0 != '9' + 1)
 	{
-		while (num1 != '9' + 1)
+	while (num1 != '9' + 1)
+	{
+		if (num1 > num0)
 		{
-			if (num1 > num0)
+			while (num2 != '9' + 1)
 			{
-				while (num2 != '9' + 1)
+				if (num2 > num1)
 				{
-					if (num2 > num1)
-					{
-						putchar(num0);
-						putchar(num1);
-						putchar(num2);
-						if (num0 == '7')
-							break;
-						putchar(',');
-						putchar(' ');
-						num2++;
-					}
-					else
-					{
-						num2++;
-					}
+					putchar(num0);
+					putchar(num1);
+					putchar(num2);
+					if (num0 == '7')
+						break;
+					putchar(',');
+					putchar(' ');
+					num2++;
 				}
-				num1++;
+				else
+				{
+					num2++;
+				}
 			}
-			else
-			{
-				num1++;
-			}
-			num2 = '0';
+			num1++;
 		}
-		num1 = '0';
-		num0++;
+		else
+		{
+			num1++;
+		}
+		num2 = '0';
+	}
+	num1 = '0';
+	num0++;
 	}
 	putchar('\n');
 	return (0);
