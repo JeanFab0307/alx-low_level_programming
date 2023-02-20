f2efbf4a096d256f6cc209c58051ec3f6c042e0e
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-02-20 08:54:58 -0800

    output with write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,6 +12,6 @@ int main (void)
 	char *mess[64];
 
 	*mess = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2 , *mess , strlen(*mess));
+	write(2, *mess, strlen(*mess));
 	return (1);
 }
