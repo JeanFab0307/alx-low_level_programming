33edc113d4f2577d89e45a83fed247a0c8783246
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-02-22 11:55:41 -0800

    Times table ProMax

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,12 @@
 /**
  *print_times_table - print the n times table starting with 0
  *@n: The limit of hte times table
+ *@r: result
  */
+void one_digit(int r);
+void two_digit(int r);
+void three_digit(int r);
+
 void print_times_table(int n)
 {
 	int i;
@@ -19,24 +24,14 @@ void print_times_table(int n)
 			while (j < n + '1')
 			{
 				if ((res / 100) != 0)
-				{
-					_putchar(res / 100 + '0');
-					_putchar((res / 10) % 10 + '0');
-				}
+					three_digit(res);
 				else if((res / 10) != 0)
-				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-				}
+					two_digit(res);
 				else
-				{
 					if (j != '0')
 					{
-						_putchar(' ');
-						_putchar(' ');
+						one_digit(res);
 					}
-				}
-				_putchar(res % 10 + '0');
 				if (j != n + '0')
 				{
 					_putchar(',');
@@ -49,4 +44,34 @@ void print_times_table(int n)
 			i++;
 		}
 	}
+}
+/**
+ *one_digit - print number of 1 char
+ *@r: the number with one or multiple digit
+ */
+void one_digit(int r)
+{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(r % 10 + '0');
+}
+/**
+ *two_digit - print number of 2 char
+ *@r: the number with one or multiple digit
+ */
+void two_digit(int r)
+{
+	_putchar(' ');
+	_putchar((r / 10) + '0');
+	_putchar(r % 10 + '0');
+}
+/**
+ *three_digit - print number of 3 char
+ *@r: the number with one or multiple digit
+ */
+void three_digit(int r)
+{
+	_putchar(r / 100 + '0');
+	_putchar((r / 10) % 10 + '0');
+	_putchar(r % 10 + '0');
 }
