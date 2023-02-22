18c090f07907c7b652fa6364bcd5a76bbaae37f4
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-02-22 08:29:12 -0800

    98 is the limit

---
+++ 0x02-functions_nested_loops/11-printto_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_to_98 - print numbers from a given number to 98
+ *@n: the number given
+ */
+void print_t_98(int n)
+{
+	int n;
+	int interval;
+
+	if (n < 0)
+		interval = 98 + (-n) + 1;
+	else
+		interval =_abs( 98 - n) + 1;
+	while (interval > 0)
+	{
+		printf("%d", 98 - interval--);
+		if (interval > -1)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
