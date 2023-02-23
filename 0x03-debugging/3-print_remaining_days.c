04250054ea61e30467f8ab196c0c1f55db904cae
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-02-23 08:05:00 -0800

    Another debugging task

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,7 +17,7 @@ void print_remaining_days(int month, int day, int year)
 		if (month == 2 && day == 60)
 		{
 		}
-		else if (month > 2 && day > 60)
+		else if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
