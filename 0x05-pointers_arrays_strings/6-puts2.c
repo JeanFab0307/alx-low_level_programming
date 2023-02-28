71d4d3da1a264c8bed48d066d92a11de4537ef90
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-02-28 07:10:35 -0800

    even char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+/**
+ * puts2 - print every even char in a string
+ * @str: the string
+ */
+void puts2(char *str)
+{
+	int l;
+	int i;
+
+	l = 0;
+	i = 0;
+	while (l <= strlen(str))
+	{
+		if (l % 2 == 0)
+			str[i++] = str[l];
+		l++;
+	}
+	l = strlen(str) - 1;
+	while (l > i--)
+		str[l--] = '\0';
+}
