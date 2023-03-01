312b1616c79fbf275a9190b1173fdff20dfb057c
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-02-28 22:26:38 -0800

    half in your bed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,10 @@ void puts_half(char *str)
 	int l;
 	int j;
 
-	j = strlen(str) / 2;
+	if (strlen(str) % 2 == 0)
+		j = strlen(str) / 2;
+	else
+		j = (strlen(str) - 1) / 2;
 	l = strlen(str) - 1;
 	while (j <= l)
 		_putchar(str[j++]);
