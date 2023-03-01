747042780745839d91acec6a66eae8b0daf5419e
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-01 07:53:59 -0800

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcpy - copy a string from a pointer to a dest
+ * @dest: the array to copy the string
+ * @src: the string copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i;
+	char *c;
+
+	c = src;
+	len = strlen(c);
+	while (i < len)
+		dest[i] = c[i++];
+	return (dest);
+}
