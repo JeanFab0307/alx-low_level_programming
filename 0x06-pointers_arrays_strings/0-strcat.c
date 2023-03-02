5119b2dd9bd8064fd4c4c0307fe48ba3f1470f73
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-01 23:38:52 -0800

    cat or dog

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: the string where we concatenates
+ * @src: the string concatenated
+ *
+ *Return: the concatenation of dest and src
+ */
+char *_strcat(char *dest, char *src);
+{
+	int len1;
+	int len2;
+	int i;
+
+	len1 = strlen(dest);
+	len2 = strlen(src);
+	i = 0;
+	while (i < len2)
+		dest[i + len1] = src[i++];
+	return (dest);
+}
