d5a61561ccf631646cd11ae14456eb8c74c7b938
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-06 10:30:52 -0800

    string_search

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,11 +16,11 @@ char *_strchr(char *s, char c)
 	i = 0;
 	j = 0;
 	ret = '\0';
-	while (!(s))
+	while (s[i])
 	{
 		if (s[i] == c)
 		{
-			while (!(s))
+			while (s[i])
 			{
 				ret[j] = s[i];
 				j++;
