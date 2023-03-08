1d3f7084174f0c54e21acb5c59dc0bdac7fd122b
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-08 11:45:45 -0800

    leoBE

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+int test_squared(int n, int i);
+/**
+ * _sqrt_recursion - return the natural squre roo of a number
+ * @n: the number
+ *
+ * Return: the natural square root of n . -1 if any
+ */
+int _sqrt_recursion(int n)
+{
+	int j;
+
+	if (n < 0)
+		return (-1);
+	j = test_squared(n, 1);
+	if (j * j > n)
+		return (-1);
+	else if (j * j == n)
+		return (j);
+}
+/**
+ * test_squared - try the power of 2 of numbers until a goal
+ * @n: the number to reach
+ * @i: the number processed
+ *
+ * Return: the power of two of n
+ */
+int test_squared(int n, int i)
+{
+	if (i * i >= n)
+		return (i);
+	test_squared(n, i + 1);
+}
