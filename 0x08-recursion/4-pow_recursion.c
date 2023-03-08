0e78c898e485206b72075797464c2570f80adbf9
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-08 04:44:49 -0800

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * pow_recursion - print the value of x^y
+ *_ pow_recursion - print the value of x^y
  * @x: the int raised tp the power
  * @y: the power
  * Return: x raised tp the powwer of y or -1 if x is negative
  */
-int pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (x < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
 	else
-		return (x * pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 }
