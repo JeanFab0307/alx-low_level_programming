cb2de981edbfaa52627460c48653ac1940c90454
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-10 09:10:19 -0800

    What is everything you got

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: the number of arg
+ * @argv: an array with the arguments
+ *
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
