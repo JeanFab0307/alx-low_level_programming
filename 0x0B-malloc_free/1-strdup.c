6b9c767d105b7e8acdb2aa251350d375c872f15c
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-14 11:47:48 -0700

    string cat and copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - returns a pointer to a new space in mem which a given string
+ * @str: the string to return
+ *
+ *Return: a duplicate of str or NULL if str =null or insufficient in memory
+ */
+char *_strdup(char *str)
+{
+	char *buffer;
+	unsigned int i;
+	unsigned int len;
+
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	buffer = malloc(len + 1);
+	if (buffer == NULL || len == 0)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		buffer[i] = str[i];
+	return (buffer);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * str_concat - concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ *Return: the concatenated string or NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *buffer;
+	unsigned int i;
+	unsigned int len1;
+	unsigned int len2;
+
+	len1 = 0;
+	len2 = 0;
+	for (i = 0; s1[i] != '\0'; i++)
+		len1++;
+	for (i = 0; s2[i] != '\0'; i++)
+		len2++;
+	buffer = malloc(len1 + len2 + 1);
+	if (buffer == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		buffer[i] = s1[i];
+	for (i = 0; i < len2; i++)
+		buffer[len1 + i] = s2[i];
+	buffer[len1 + len2 + 1] = '\0';
+	return (buffer);
+}
