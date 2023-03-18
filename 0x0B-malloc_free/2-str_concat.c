997175de6359583786b2f37450179c4e119953ff
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-18 04:16:01 -0700

    Function _concat corrected for NULL cases in arg

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,17 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	len2 = 0;
 	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1 == NULL)
+			break;
 		len1++;
+	}
 	for (i = 0; s2[i] != '\0'; i++)
+	{
+		if (len2 == NULL)
+			break;
 		len2++;
+	}
 	buffer = malloc(len1 + len2 + 1);
 	if (buffer == NULL)
 		return (NULL);
