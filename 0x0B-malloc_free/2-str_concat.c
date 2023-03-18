fa10e8e03638e377b04945a0608d688a22b764f7
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-18 04:30:26 -0700

    last changes for function str _concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,35 +14,23 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len1;
 	unsigned int len2;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len1 = 0;
 	len2 = 0;
 	for (i = 0; s1[i] != '\0'; i++)
-	{
-		if (s1 == NULL)
-			break;
 		len1++;
-	}
 	for (i = 0; s2[i] != '\0'; i++)
-	{
-		if (s2 == NULL)
-			break;
 		len2++;
-	}
 	buffer = malloc(len1 + len2 + 1);
 	if (buffer == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
-	{
-		if (s1 == NULL)
-			break;
 		buffer[i] = s1[i];
-	}
 	for (i = 0; i < len2; i++)
-	{
-		if (s2 == NULL)
-			break;
 		buffer[len1 + i] = s2[i];
-	}
 	buffer[len1 + len2 + 1] = '\0';
 	return (buffer);
 }
