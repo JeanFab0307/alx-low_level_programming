885a00967226b58355bd572ab39f4becf80f6e2f
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-20 14:55:30 -0700

    function Print_dog changed for NULL cases

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * print_dog - print a struct dog: name, agew, owner
@@ -6,7 +7,23 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+	{
+		printf("");
+	}
+	else
+	{
+		if (d->name == NULL)
+			printf("(nil)");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age == NULL)
+			printf("(nil)");
+		else
+			printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("(nil)");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
