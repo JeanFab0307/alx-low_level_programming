d8ebc0380b8ae1efe90c783d2711905c2af85513
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-21 00:37:32 -0700

    print_dog func fixed: cases of name and owner NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,16 +13,11 @@ void print_dog(struct dog *d)
 	else
 	{
 		if (d->name == NULL)
-			printf("(nil)");
-		else
-			printf("Name: %s\n", d->name);
-		if (!(d->age))
-			printf("(nil)");
-		else
-			printf("Age: %f\n", d->age);
+			d->name = "(nil)";
 		if (d->owner == NULL)
-			printf("(nil)");
-		else
-			printf("Owner: %s\n", d->owner);
+			d->owner = "(nil)";
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
 }
