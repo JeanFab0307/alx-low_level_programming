e5e14d53d77a2648068ad2b2925873648fdf3607
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-22 13:15:55 -0700

    Function array_iterator added and header file modified

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - execute a function given on an array
+ * @array: the given array
+ * @size: size of the array
+ * @action: the function executed on the array
+ *
+ *Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size || array[i] != '\0'; i++)
+		(*action)(array[i]);
+}
