27db1f0f44de155860d3f858f5329155e93cb5d3
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-24 08:58:27 -0700

    Func sum_them_all fixed: Betty style issue on header filecorrected

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_C
 
-#define VARIADIC_FUNCTION_C
+#define VARIADIC_FUNCTIONS_C
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
