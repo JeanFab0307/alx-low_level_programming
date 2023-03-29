cc66c230e88cf94a852c894d238768f323c84297
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-29 10:42:53 -0700

    File 104-fibonacci.c added

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+	double fib1 = 1;
+	double fib2 = 2;
+	double temp;
+	int lim = 98;
+
+	while (i < lim)
+	{
+		if (i != 0)
+			printf(", ");
+		printf("%.0f", fib1);
+		i++;
+		temp = fib1;
+		fib1 = fib2;
+		fib2 = temp + fib1;
+	}
+	printf("\n");
+	return (0);
+}
