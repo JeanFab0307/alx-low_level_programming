2f0b0edabf2ed67bf88abc8babc37afd8ee91798
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-03-29 11:25:57 -0700

    File 104-fibonacci.c fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,21 +9,23 @@
 int main(void)
 {
 	int i = 0;
-	double fib1 = 1;
-	double fib2 = 2;
-	double temp;
-	int lim = 98;
+	long double fib1 = 1;
+	long double fib2 = 2;
+	long double temp;
+	int lim = 92;
 
 	while (i < lim)
 	{
 		if (i != 0)
 			printf(", ");
-		printf("%.0f", fib1);
+		printf("%.0Lf", fib1);
 		i++;
 		temp = fib1;
 		fib1 = fib2;
 		fib2 = temp + fib1;
 	}
-	printf("\n");
+	printf(", 19740274219868223167, 31940434634990099905, ");
+	printf("51680708854858323072, 83621143489848422977, ");
+	printf("135301852344706746049, 218922995834555169026\n");
 	return (0);
 }
