40983755883e3d52b1b18cebfd6049b6e8fd8104
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-04-05 10:43:52 -0700

    6-pop_listint.c fixed

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,7 +12,8 @@ int pop_listint(listint_t **head)
 {
 	int i;
 	listint_t *temp;
-	if (head == NULL)
+
+	if (*head == NULL || head == NULL)
 		return (0);
 	i = (*(*head)).n;
 	temp = (*(*head)).next;
