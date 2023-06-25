e23e62138472b58fdd81652ef5f2b4644b969395
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-06-25 09:17:38 -0700

    Fix prev value

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,15 +11,17 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *tmp;
+	dlistint_t *new;
 
-	tmp = malloc(sizeof(dlistint_t));
-	if (tmp == NULL)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
 
-	tmp->n = n;
-	tmp->prev = NULL;
-	tmp->next = *head;
-	*head = tmp;
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head)
+		(*head)->prev = new;
+	*head = new;
 	return (*head);
 }
