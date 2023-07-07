18dc86c91f18780db724685e75b4fd72256f487b
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-07-07 04:50:27 -0700

    Add Betty documentation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - create a hash table
+ * @size: Its size
+ *
+ * Return: A pointer to the newly created hash table
+ * Or Null if failed
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
@@ -10,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t*) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
 		free(ht);
